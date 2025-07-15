a0a2f6cbaba0cacd3e88a0687632bdafb3ccff18
Tech2Hack <benugopal2005@gmail.com>
2025-07-15 21:00:03 +0530

    Initial commit by Tech2Hack

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { HomePage } from "./containers/HomePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { CustomerAccessPage } from "./containers/customerAccessPage";
 
-added functional component 
 function App() {
   return (
     <div className="App">
@@ -22,20 +21,4 @@ function App() {
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route
-            path="/customer/access/:action"
-            exact
-            component={CustomerAccessPage}
-          />
-        </Switch>
-      </Router>
-    </div>
-  );
-}
 export default App;
